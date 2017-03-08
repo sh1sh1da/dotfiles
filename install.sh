e8cd30e4f1c6965ddc81db44579d8971d55f21cd
Shintaro Ishida <shintaro@ShintaronoMacBook-Pro.local>
2017-03-08 15:11:20 +0900

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+DOTFILES=(.spacemacs .zshrc)
+
+for f in ${DOTFILES[@]}
+do
+    ln -fnsv ./$f ~/$f
+done
+
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+for f in `ls ./spacemacs_private/`
+do
+    ln -s ./spacemacs_private/$f ~/.emacs.d/private/$f
+done
+
 if [ ! -d ~/.zplug ]; then
-  curl -sL zplug.sh/installer | zsh
+    curl -sL zplug.sh/installer | zsh
 fi
