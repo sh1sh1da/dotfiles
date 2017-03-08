2d764564cecf4082e5c73ed72f84b10ac12a6d10
Shintaro Ishida <shintaro@ShintaronoMacBook-Pro.local>
2017-03-08 16:19:39 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+DOTFILES=(.spacemacs .zshrc)
+WORKDIR=`pwd`
+
+for f in ${DOTFILES[@]}
+do
+    ln -fnsv $WORKDIR/$f ~/$f
+done
+
+if [ -e ~/.emacs.d ]; then
+    mv ~/.emacs.d ~/.emacs.d.old
+fi
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+for f in `ls $WORKDIR/spacemacs_private/`
+do
+    ln -s $WORKDIR/spacemacs_private/$f ~/.emacs.d/private/$f
+done
+
 if [ ! -d ~/.zplug ]; then
-  curl -sL zplug.sh/installer | zsh
+    curl -sL zplug.sh/installer | zsh
 fi
